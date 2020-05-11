4e694002a909cb0b3a504e08053740e7fa973d6d
Ana Medrano <amedrano.fernandez@gmail.com>
2020-05-10 18:57:33 -0700

    seeded the file

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,27 +1,33 @@
 var db = require("../models");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
 module.exports = function (app) {
+
+  // app.get("/api/workouts", (req,res) => {
+  //   db.Workout.create(req.body,{})
+  // }))
   
-  app.put("/api/workouts/:id", (res => {
-    db.Workout.update({
-      _id: mongojs.ObjectId(req.params.id)
-    }, {
-      $set: {
-        type: req.body.type,
-        name: req.body.name,
-        distance: req.body.distance,
-        duration: req.body.duration,
-        weight: req.body.weight,
-        sets: req.body.sets,
-        reps: req.body.reps,
-        date: Date.now()
-      }
-    }).then(result => {
-      console.log(result)
-      res.json(result)
-    }).catch(err => {
-      res.json(err)
-    });
-  }));
+  // app.put("/api/workouts/:id", (res => {
+  //   db.Workout.update({
+  //     _id: mongojs.ObjectId(req.params.id)
+  //   }, {
+  //     $set: {
+  //       type: req.body.type,
+  //       name: req.body.name,
+  //       distance: req.body.distance,
+  //       duration: req.body.duration,
+  //       weight: req.body.weight,
+  //       sets: req.body.sets,
+  //       reps: req.body.reps,
+  //       date: Date.now()
+  //     }
+  //   }).then(result => {
+  //     console.log(result)
+  //     res.json(result)
+  //   }).catch(err => {
+  //     res.json(err)
+  //   });
+  // }));
 
 }
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-const db = require("./models");
+// const db = require("./models");
 
 const app = express();
 
@@ -16,7 +16,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 require("./routes/api-routes.js")(app);
 require("./routes/html-routes.js")(app);
 
